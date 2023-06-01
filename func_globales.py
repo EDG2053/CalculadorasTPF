dc6b77473612eabd6f9292e303478c3184cfd476
David <edg20530@gmail.com>
2023-06-01 20:04:03 -0300

    Funciones medianamente documentadas

--- func_globales.py
+++ func_globales.py
@@ -4,10 +4,17 @@
 #
 
 def numero_valido(num):
+    """
+        Recibe un numero, si este da error al convertirlo lo vuelve a pedir hasta que 
+        sea un numero.
+
+        retorna un int si el numero no tiene decimales(.0) y float si los tiene(.3)
+    """
+
     try:
         float(num)
     except ValueError:
-        num=input("Error, tiene que ingresar un numero:")
+        num=input("Error, tiene que ingresar un numero: ")
     else:
         num=float(num)
         if num%1 == 0:
@@ -17,16 +24,18 @@ def numero_valido(num):
 
 def operador_valido(operador):
     """
-        
-    """
+        Verifica si el operador es valido ( si es +, -, x, / )
+        Si no es valido lo vuelve a pedir hasta que lo sea
 
+        retorna el operador
+    """
 
     operador= operador.strip() # strip elimina los espacios 
     operadores=["+","-","x", "/","="]        
     
     while not (operador in operadores):
         print("El operador debe ser |+|, |-|, |x|(equis), |/| o |=| ")
-        operador=input("\n Reingrese operador: ")
+        operador=input("\n Reingrese operador: ")# \n hace un salto de linea
 
     return operador
 
@@ -39,6 +48,12 @@ def operador_valido(operador):
 #
 
 def operacion(num1, operador, num2):
+    """
+        Realiza una operacion segun el operador que reciba( +, -, x, / )
+        Retorna el resultado
+
+        Usar solo luego de validar los numeros y operadores
+    """
     sumar=operador == "+"
     restar=operador == "-"
     dividir=operador == "/"
@@ -60,4 +75,3 @@ def operacion(num1, operador, num2):
 # ---------FIN Func. de Realizar operaciones ---------
 #
 
-print(operador_valido(input("op: ")))
