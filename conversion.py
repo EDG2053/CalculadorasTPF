587421edfecfe6ae4f9f4fe8633fefc2d871dc51
David <edg20530@gmail.com>
2023-06-04 01:23:45 -0300

    para la casa

--- conversion.py
+++ conversion.py
@@ -1,6 +1,13 @@
 from func_globales import numero_valido, operador_valido, operacion
 
+def a_octal():
+    pass
 
+def a_hexa():
+    pass
+
+def a_bin():
+    pass
 
 def Calculadora_conversiones():
     pass
