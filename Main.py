78fe53a0c55f287ed752deb7efe9225f2b6febd7
David G <edg20530@gmail.com>
2023-06-13 19:04:15 -0300

    actualizacion 2.0

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 from func_globales import numero_valido as num_val
 from clasica import Calculadora_Clasica 
 from fraccion import Calculadora_fracciones
-from conversion import Calculadora_conversiones
+from conversion import a_hexa, a_binario, a_octal
 
 print("""
     Para una mejor experiencia le sugerimos 
@@ -11,10 +11,28 @@ print("""
 
                 1 - Clasica
                 2 - Fracciones 
-                3 - COnversiones
+                3 - Conversiones
                 4 - Salir / off
 
     """)
 opcion= num_val( input("     Ingrese el numero de calculadora:  ") )
 
+while opcion !=4:
+    if opcion == 1:
+        Calculadora_Clasica()
+    elif opcion == 2:
+        Calculadora_fracciones()
+    elif opcion == 3:
+        pass
 
+    print("""   
+        ---- MENU CALCULADORAS ----
+
+                1 - Clasica
+                2 - Fracciones 
+                3 - Conversiones
+                4 - Salir / off
+    """)
+
+    opcion= num_val( input("     Ingrese el numero de calculadora:  ") )
+    
